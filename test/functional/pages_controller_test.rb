98cd6f159a0b785ca2ad5286595bf6572f684c14
Swapnesh Sinha <swapnesh20032003@gmail.com>
2013-02-25 11:34:14 +0530

    2 commit - Added pages by creating page controller

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get Home" do
+    get :Home
+    assert_response :success
+  end
+
+  test "should get About" do
+    get :About
+    assert_response :success
+  end
+
+  test "should get Contact" do
+    get :Contact
+    assert_response :success
+  end
+
+  test "should get Drop_Down" do
+    get :Drop_Down
+    assert_response :success
+  end
+
+end
