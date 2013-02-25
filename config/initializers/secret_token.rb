2ea0a0a06395af01675a079d1dc1b9a99b0e56c1
Swapnesh Sinha <swapnesh20032003@gmail.com>
2013-02-25 11:04:14 +0530

    First commit - Added all basic rails folders/structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addbootstrap::Application.config.secret_token = '32432bcb557077faece7f21b44577829545794afe2dd0cd4fe097e1cea22130aef32a8dd9904a1b3008cab26882675cd89897b5214295567a8b69348146e1db1'
